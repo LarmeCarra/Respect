7d4953fe01e5eb5b3b639c88a26a8b17b19d29a2
LarmeCarra <129941241+LarmeCarra@users.noreply.github.com>
2026-05-07 16:53:38 +0800

    Add files via upload

---
+++ homes.js
@@ -0,0 +1,9 @@
+function startmusic(){
+    document.getElementById("nk").style.display = "none";
+    var m= new Audio("glitter.mp3");
+    var n= new Audio("secret.mp3");
+    var b= new Audio("respect.mp3");
+    m.play();
+    n.play();
+    b.play();
+}
